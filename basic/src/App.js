bc822f13cb846141b4e4c29320c79c879d71fc3c
jaeml00 <jaeml0630@gmail.com>
2023-06-22 14:43:18 +0900

    1

---
+++ basic/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = '엘리';
+  const list = ['우유','딸기','바나나', '요거트'];
+  return (
+    <>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
+      <h2>hello!</h2>
+        <ul>
+          {
+            list.map((item)=>(
+              <li>{item}</li>
+            ))
+          }
+        </ul>
+        <img
+        style={{width:'210px' , height: '200px'}}
+        src="https://s3.marpple.co/files/u_218933/2020/1/original/14474423ccd1acb63fab43dc936ab01302c64b547577e2e.png"
+        />
+    </>
+  );
+  
+}
+
+export default App;
