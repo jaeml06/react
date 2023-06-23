1192fa45502867cec1f93b88e4a775447469c158
jaeml00 <jaeml0630@gmail.com>
2023-06-23 13:50:18 +0900

    0623

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -1,24 +1,17 @@
 
 import './App.css';
+import Profile from './components/Profile';
 
 function AppProfile() {
-  const name = '엘리';
-  const list = ['우유','딸기','바나나', '요거트'];
+
   return (
     <>
-      <h1 className='orange'>{`Hello! ${name}`}</h1>
-      <h2>hello!</h2>
-        <ul>
-          {
-            list.map((item)=>(
-              <li>{item}</li>
-            ))
-          }
-        </ul>
-        <img
-        style={{width:'200px' , height: '200px'}}
-        src="https://s3.marpple.co/files/u_218933/2020/1/original/14474423ccd1acb63fab43dc936ab01302c64b547577e2e.png"
-        />
+      <Profile image='https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80'
+      name='James Kim' title='프론트엔드 개발자'/>
+      <Profile image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
+      name='Anna Yong' title='백엔드 개발자'/>
+      <Profile image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
+      name='Bob yu' title='프론트엔드 개발자'/>
     </>
   );
   
