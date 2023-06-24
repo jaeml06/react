2d33c30c46027d2f2472f4afdbc70009d85eb809
jaeml00 <jaeml0630@gmail.com>
2023-06-25 00:06:33 +0900

    0625

--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import Avater from './Avatar';
+import Avartar from './Avartar';
 
-export default function Profile({image, name, title, isNew}) {
-    return (
-        <div className='profile'>
-            <Avater image={image} isNew={isNew}/>
-            
-            <h1>{name}</h1>
-            <p>{title}</p>
-        </div>
-    );
+export default function Profile({ image, name, title, isNew }) {
+  return (
+    <div className='profile'>
+      <Avartar image={image} isNew={isNew} />
+      <h1>{name}</h1>
+      <p>{title}</p>
+    </div>
+  );
 }
-
-
