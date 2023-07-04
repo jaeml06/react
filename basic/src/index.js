22aa2b6f593d3d080d9feaab43c57cb7d33da76d
jaeml00 <jaeml0630@gmail.com>
2023-07-05 00:06:06 +0900

    0704

--- basic/src/index.js
+++ basic/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import AppMentors from './AppMentors';
+import AppMentorsImmer from './AppMentorsImmer';
+import AppForm from './AppForm';
+import AppWrap from './AppWrap';
+import AppCard from './AppCard';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppMentors />
+    <AppCard />
   </React.StrictMode>
 );
 
